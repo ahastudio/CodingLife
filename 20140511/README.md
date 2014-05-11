94c5afd4b81a64a480330d367bec1bca277021c4
ahastudio <ahastudio@gmail.com>
2014-05-11 22:10:15 +0900

    Longest Substring Without Repeating Characters 문제 풀이

---
+++ 20140511/README.md
@@ -0,0 +1,3 @@
+Longest Substring Without Repeating Characters 문제: http://j.mp/1jfVJyh
+
+스크린캐스트: http://www.youtube.com/watch?v=kRaofEZOhVI
---
+++ 20140511/spec/longest_substring_spec.rb
@@ -0,0 +1,34 @@
+class String
+  def letters
+    split('')
+  end
+
+  def substring_without_repeating
+    substring = ''
+    letters.each do |c|
+      break if substring.include?(c)
+      substring << c
+    end
+    substring
+  end
+
+  def longest_substring
+    if size > 1
+      [substring_without_repeating, self[1..-1].longest_substring].
+        max_by{|i| i.size}
+    else
+      self
+    end
+  end
+end
+
+puts "-----------"
+
+describe "String#longest_subsring" do
+  it "finds the longest substring without repeating characters" do
+    expect("abcabcbb".longest_substring).to eq("abc")
+    expect("bbbbb".longest_substring).to eq("b")
+    expect("bbba".longest_substring).to eq("ba")
+    expect("abcabcbbabcd".longest_substring).to eq("abcd")
+  end
+end
