1c9fb9db37c1cf1eea8215ac4fc75b239fba3add
Ashal aka JOKER <ahastudio@gmail.com>
2014-12-28 23:26:05 +0900

    Ruby와 Python으로 3n+1 문제 풀이

---
+++ 20141227/Python/test.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+cache = {}
+
+
+def next_number(number):
+  if number % 2 == 0:
+    return number / 2
+  else:
+    return (number * 3) + 1
+
+def f(number):
+  if number == 1:
+    return 1
+  if number not in cache:
+    cache[number] = 1 + f(next_number(number))
+  return cache[number]
+
+def max_length(first, last):
+  return max(f(i) for i in range(first, last + 1))
+
+
+class TestThreeNplusOne(unittest.TestCase):
+  def test_max_length(self):
+    self.assertEqual(20, max_length(1, 10))
+    self.assertEqual(125, max_length(100, 200))
+    self.assertEqual(89, max_length(201, 210))
+    self.assertEqual(174, max_length(900, 1000))
+    self.assertEqual(351, max_length(1, 100000))
+
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ 20141227/Ruby/test.rb
@@ -0,0 +1,40 @@
+$cache = {}
+
+class Integer
+  def length_3n_plus_1
+    if self == 1
+      1
+    else
+      $cache[self] ||= 1 + (even? ? (self / 2).length_3n_plus_1 : (self * 3 + 1).length_3n_plus_1)
+    end
+  end
+end
+
+class TheeNplusOne
+  def initialize(a, b=nil)
+    @a = a
+    @b = b
+    @cache = {}
+  end
+
+  def length
+    (@a..@b).map { |i| i.length_3n_plus_1 }.max
+  end
+end
+
+puts "-----------------------"
+
+describe "3n + 1" do
+  it "returns max length" do
+    expect(TheeNplusOne.new(1, 10).length).to eq(20)
+    expect(TheeNplusOne.new(100, 200).length).to eq(125)
+    expect(TheeNplusOne.new(201, 210).length).to eq(89)
+    expect(TheeNplusOne.new(900, 1000).length).to eq(174)
+    expect(TheeNplusOne.new(1, 100000).length).to eq(351)
+  end
+
+  it "returns 3n+1 length" do
+    expect(3.length_3n_plus_1).to eq(8)
+    expect(10.length_3n_plus_1).to eq(7)
+  end
+end
