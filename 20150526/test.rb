d138e8afe7b94c29da47b53c7055754d47ec6310
Ashal aka JOKER <ahastudio@gmail.com>
2015-05-26 07:45:43 +0900

    Five programming problems every Software Engineer should be able to solve in less than 1 hour 문제 1번 풀이

---
+++ 20150526/test.rb
@@ -0,0 +1,35 @@
+def sum_using_for(list)
+  sum = 0
+  for i in list
+    sum += i
+  end
+  sum
+end
+
+def sum_using_while(list)
+  sum = 0
+  while list.size > 0
+    sum += list.pop
+  end
+  sum
+end
+
+def sum_using_recursion(list)
+  def recur(acc, list)
+    list.empty? ? acc : recur(acc + list.first, list[1..-1])
+  end
+
+  recur(0, list)
+end
+
+puts '---------------------------------------'
+
+describe "Five programming problems" do
+  describe "Sum" do
+    it "calculate sum of list" do
+      expect(sum_using_for([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq(55)
+      expect(sum_using_while([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq(55)
+      expect(sum_using_recursion([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq(55)
+    end
+  end
+end
