a15a19a2eeb17996dcd7d5f88a501ab4739c2bf3
Ashal aka JOKER <ahastudio@gmail.com>
2015-05-26 07:49:43 +0900

    Five programming problems 문제 2번 풀이

--- 20150526/test.rb
+++ 20150526/test.rb
@@ -22,6 +22,10 @@ def sum_using_recursion(list)
   recur(0, list)
 end
 
+def combine(a, b)
+  a.zip(b).flatten
+end
+
 puts '---------------------------------------'
 
 describe "Five programming problems" do
@@ -30,6 +34,12 @@ describe "Five programming problems" do
       expect(sum_using_for([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq(55)
       expect(sum_using_while([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq(55)
       expect(sum_using_recursion([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to eq(55)
+    end
+  end
+
+  describe "Combine two lists" do
+    it "combines two lists by alternatingly taking elements" do
+      expect(combine([:a, :b, :c], [1, 2, 3])).to eq([:a, 1, :b, 2, :c, 3])
     end
   end
 end
