113bf334ea62107917f6ca427fe956a3fce8bbdb
Ashal aka JOKER <ahastudio@gmail.com>
2015-08-03 21:56:45 +0900

    FP in Scala 7장 코딩

---
+++ 20150803/src/test/scala/chapter7.scala
@@ -0,0 +1,117 @@
+package parallelism
+
+import org.scalatest._
+import java.util.concurrent._
+import java.util.concurrent.atomic._
+import akka.actor.Actor
+
+sealed trait Future[A] {
+  private[parallelism] def apply(k: A => Unit): Unit
+}
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def run[A](es: ExecutorService)(p: Par[A]): A = {
+    val ref = new AtomicReference[A]
+    val latch = new CountDownLatch(1)
+    p(es) { a => ref.set(a); latch.countDown }
+    latch.await
+    ref.get
+  }
+
+  def unit[A](a: A): Par[A] =
+    (es: ExecutorService) => new Future[A] {
+      def apply(cb: A => Unit): Unit =
+        cb(a)
+    }
+
+  def map2[A, B, C](p: Par[A], p2: Par[B])(f: (A, B) => C): Par[C] =
+    es => new Future[C] {
+      def apply(cb: C => Unit): Unit = {
+        var ar: Option[A] = None
+        var br: Option[B] = None
+
+        val combiner = new Actor {
+          def receive = {
+            case Left(a: A) => br match {
+              case None => ar = Some(a)
+              case Some(b) => eval(es)(cb(f(a, b)))
+            }
+            case Right(b: B) => ar match {
+              case None => br = Some(b)
+              case Some(a) => eval(es)(cb(f(a, b)))
+            }
+          }
+        }
+
+        p(es)(a => combiner.sender ! Left(a))
+        p2(es)(b => combiner.sender ! Right(b))
+      }
+    }
+
+  def dealy[A](fa: => Par[A]): Par[A] =
+    es => fa(es)
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => new Future[A] {
+      def apply(cb : A => Unit): Unit =
+        eval(es)(a(es)(cb))
+    }
+
+  def eval(es: ExecutorService)(r: => Unit): Unit =
+    es.submit(new Callable[Unit] { def call = r })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    (a: A) => lazyUnit(f(a))
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ps match {
+    case Nil => unit(Nil)
+    case h :: t => map2(h, fork(sequence(t)))((a, b) => a :: b)
+  }
+
+  def sequence2[A](ps: List[Par[A]]): Par[List[A]] =
+    ps.foldRight(unit(List.empty[A]))((a, b) => map2(a, b)(_ :: _))
+
+  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] =
+    map2(parMap(as)(a => (a, f(a))), unit(())) { (as, _) =>
+      as.foldRight(List.empty[A]) {
+        case ((h, true), t) => h :: t
+        case ((h, false), t) => t
+      }
+    }
+
+  def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean =
+    run(e)(p) == run(e)(p2)
+}
+
+class TestChapter7 extends FlatSpec with Matchers {
+  val es: ExecutorService = Executors.newCachedThreadPool
+
+  it should "check many executor" in {
+    val a = Par.lazyUnit(42 + 1)
+    // FAIL - val es: ExecutorService = Executors.newSingleThreadExecutor
+    // FAIL - val es: ExecutorService = Executors.newFixedThreadPool(1)
+    val es: ExecutorService = Executors.newFixedThreadPool(2)
+    Par.equal(es)(a, Par.fork(a)) should be (true)
+  }
+
+  it should "check single thread" in {
+    val a = Par.lazyUnit(42 + 1)
+    val es: ExecutorService = Executors.newSingleThreadExecutor
+    Par.run(es)(a) should be (42 + 1)
+  }
+
+  it should "check fixed thread pool" in {
+    val a = Par.lazyUnit(42 + 1)
+    val es: ExecutorService = Executors.newFixedThreadPool(1)
+    Par.run(es)(a) should be (42 + 1)
+  }
+}
