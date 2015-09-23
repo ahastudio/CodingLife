92811f08c5de652af493106db9d58c4807e16e1b
Ashal aka JOKER <ahastudio@gmail.com>
2015-09-23 17:29:59 +0900

    오델로 게임 서버 기본 코드 작성.

---
+++ 20150923/application.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, render_template
+
+from game import Board
+
+import json
+
+
+board = Board()
+players = []
+ready = []
+playing = False
+history = []
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/enter')
+def enter():
+    key = request.args.get('key', '')
+    if len(players) < 2 and len(key) > 0 and key not in players:
+        players.append(key)
+    return '%d' % len(players)
+
+@app.route('/leave')
+def leave():
+    key = request.args.get('key', '')
+    if key in players:
+        players.remove(key)
+        ready.remove(key)
+    return '%d' % len(players)
+
+@app.route('/start')
+def start():
+    key = request.args.get('key', '')
+    if key not in players:
+        return ''
+    if key not in ready:
+        ready.append(key)
+    playing = len(ready) is 2
+    history = []
+    return json.dumps(dict(start=playing))
+
+@app.route('/put')
+def put():
+    key = request.args.get('key', '')
+    if key not in players:
+        return ''
+    x = int(request.args.get('x', 0))
+    y = int(request.args.get('y', 0))
+    history.append(dict(player=players.index(key), x=x, y=y))
+    return ''
+
+@app.route('/get')
+def get():
+    key = request.args.get('key', '')
+    if key not in players:
+        return ''
+    return json.dumps([[i['x'], i['y']] for i in history
+                       if i['player'] is not players.index(key)])
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 20150923/game.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class Board:
+    pass
+
+
+class TestBoard(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
