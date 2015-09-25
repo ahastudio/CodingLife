00f54920e737ab7228b1ebd53dc8bfa3104f36fa
Ashal aka JOKER <ahastudio@gmail.com>
2015-09-25 14:07:04 +0900

    돌을 놓았을 때, 끼인 돌 flip.

--- 20150923/othello.py
+++ 20150923/othello.py
@@ -1,5 +1,7 @@
 import unittest
 
+from itertools import permutations
+
 
 BLANK = '.'
 BLACK = 'X'
@@ -7,6 +9,7 @@ WHITE = 'O'
 WALL = '#'
 WIDTH = 8
 HEIGHT = 8
+DIRECTIONS = [x for x in permutations([-1, 0, 1], 2) if x is not [0, 0]]
 
 
 class Board:
@@ -23,6 +26,9 @@ class Board:
     def put(self, stone, x, y):
         if self.valid(x, y):
             self.cells[y * HEIGHT + x] = stone
+            for direction in DIRECTIONS:
+                for pos in self.get_flips(stone, x, y, direction):
+                    self.cells[pos[1] * HEIGHT + pos[0]] = stone
 
     def get(self, x, y):
         if not self.valid(x, y):
@@ -31,6 +37,19 @@ class Board:
 
     def valid(self, x, y):
         return 0 <= x < WIDTH and 0 <= y < HEIGHT
+
+    def get_flips(self, stone, x, y, direction):
+        other = (stone is BLACK) and WHITE or BLACK
+        dx, dy = direction
+        def iter(x, y, positions):
+            cell = self.get(x, y)
+            if cell is stone:
+                return positions
+            if cell is other:
+                positions.append([x, y])
+                return iter(x + dx, y + dy, positions)
+            return []
+        return iter(x + dx, y + dy, [])
 
     def __str__(self):
         return '\n'.join(''.join(self.cells[i:i + WIDTH])
@@ -61,6 +80,17 @@ class TestBoard(unittest.TestCase):
 
     def test_get_outside(self):
         self.assertEqual(WALL, self.board.get(100, 3))
+
+    def test_get_flips(self):
+        self.assertEqual([[3, 3]], self.board.get_flips(BLACK, 2, 3, [1, 0]))
+        self.assertEqual([], self.board.get_flips(BLACK, 2, 3, [-1, 0]))
+        self.assertEqual([], self.board.get_flips(BLACK, 1, 3, [1, 0]))
+        self.assertEqual([], self.board.get_flips(BLACK, 4, 3, [-1, 0]))
+
+    def test_put_and_flip(self):
+        self.assertEqual(WHITE, self.board.get(3, 3))
+        self.board.put(BLACK, 2, 3)
+        self.assertEqual(BLACK, self.board.get(3, 3))
 
 
 if __name__ == '__main__':
