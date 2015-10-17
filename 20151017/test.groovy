22864cec8d9bae484a5b2bc34239aefb9dce7453
Ashal aka JOKER <ahastudio@gmail.com>
2015-10-18 00:32:01 +0900

    Groovy Meta-programming.
    http://www.groovy-lang.org/metaprogramming.html

--- 20151017/test.groovy
+++ 20151017/test.groovy
@@ -9,8 +9,21 @@ def fib(n) {
 }
 */
 
-def fib(n) {
-  (1..n).inject([0, 1]) { a, e -> [a[1], a[0] + a[1]] }[0]
-}
+def fib = { n -> (1..n).inject([0, 1]) { a, e -> [a[1], a[0] + a[1]] }[0] }
 
-assert [1, 1, 2, 3, 5, 8, 13, 21] == (1..8).collect { fib(it) }
+def n = 8
+def expected = [1, 1, 2, 3, 5, 8, 13, 21]
+
+assert expected == (1..n).collect { fib(it) }
+
+// Destructuring using Meta-programming
+
+def (first, second) = [1, 2, 3]
+assert [1, 2] == [first, second]
+
+java.util.List.metaClass.destructure = {-> [get(0), subList(1, size())] }
+
+def (head, tail) = [1, 2, 3].destructure()
+assert [1, [2, 3]] == [head, tail]
+
+println "OK!"
