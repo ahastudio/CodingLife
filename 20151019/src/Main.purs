23117c5e5cdd011a6b1658ba4c8fb7858e956106
Ashal aka JOKER <ahastudio@gmail.com>
2015-10-19 23:57:43 +0900

    PureScript 테스트.

---
+++ 20151019/src/Main.purs
@@ -0,0 +1,6 @@
+module Main where
+
+import Control.Monad.Eff.Console
+
+main = do
+  log "OK!"
---
+++ 20151019/test/Main.purs
@@ -0,0 +1,6 @@
+module Test.Main where
+
+import Test.Assert (assert)
+
+main = do
+  assert (2 == 1 + 1)
