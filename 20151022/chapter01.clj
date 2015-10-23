81cabeb2f9983146ecf9c2c226ecc3a762bf8d17
Ashal aka JOKER <ahastudio@gmail.com>
2015-10-23 21:18:42 +0900

    Clojure Macro 활용.

--- 20151022/chapter01.clj
+++ 20151022/chapter01.clj
@@ -2,6 +2,8 @@
 (remove-ns 'test)
 (ns test)
 
+; SICP 스터디에서 분수와 고정소수점 등에 대해서 이야기함.
+
 ; 분수
 
 (* (float (/ 1 3)) 3)
@@ -50,3 +52,47 @@ counter
 (reset! counter 0)
 (gcd 206 #(-> 40))
 counter
+
+; https://clojuredocs.org/clojure.core/defmacro
+
+(defmacro gcd [a b]
+  `(if (= ~b 0)
+     ~a
+     (eval '(gcd ~b (remainder ~a ~b)))))
+
+(reset! counter 0)
+(gcd 206 40)
+counter
+
+; [번외편] 연습문제 1.6에서 나온 new-if를 Macro로 구현할 수 있을까?
+
+(defn new-if [condition then else]
+  (cond condition then
+        :else else))
+
+(new-if (> 2 1) "T" "F")
+(new-if (< 2 1) "T" "F")
+
+(defn iter [a b counter]
+  (new-if (> a b)
+          counter
+          (iter (+ a 1) b (+ counter 1))))
+
+; (iter 3 10 0)
+; java.lang.StackOverflowError: null
+; -> 무한반복!
+
+(defmacro new-if [condition then else]
+  `(cond ~condition ~then
+         :else ~else))
+
+(new-if (> 2 1) "T" "F")
+(new-if (< 2 1) "T" "F")
+
+; Macro를 쓰기 때문에, 여기서 iter를 다시 정의해야 함.
+(defn iter [a b counter]
+  (new-if (> a b)
+          counter
+          (iter (+ a 1) b (+ counter 1))))
+
+(iter 3 10 0)
