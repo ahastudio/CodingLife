f38455b13aab0c3146f726c0eed249c77ea3e11f
Ashal aka JOKER <ahastudio@gmail.com>
2015-10-24 09:52:42 +0900

    new-if 실험 코드를 SICP 실습에서 분리.

--- 20151022/chapter01.clj
+++ 20151022/chapter01.clj
@@ -63,36 +63,3 @@ counter
 (reset! counter 0)
 (gcd 206 40)
 counter
-
-; [번외편] 연습문제 1.6에서 나온 new-if를 Macro로 구현할 수 있을까?
-
-(defn new-if [condition then else]
-  (cond condition then
-        :else else))
-
-(new-if (> 2 1) "T" "F")
-(new-if (< 2 1) "T" "F")
-
-(defn iter [a b counter]
-  (new-if (> a b)
-          counter
-          (iter (+ a 1) b (+ counter 1))))
-
-; (iter 3 10 0)
-; java.lang.StackOverflowError: null
-; -> 무한반복!
-
-(defmacro new-if [condition then else]
-  `(cond ~condition ~then
-         :else ~else))
-
-(new-if (> 2 1) "T" "F")
-(new-if (< 2 1) "T" "F")
-
-; Macro를 쓰기 때문에, 여기서 iter를 다시 정의해야 함.
-(defn iter [a b counter]
-  (new-if (> a b)
-          counter
-          (iter (+ a 1) b (+ counter 1))))
-
-(iter 3 10 0)
