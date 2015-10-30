58189d5b02d7e22e836d8a9b7da7a2100a68f951
Ashal aka JOKER <ahastudio@gmail.com>
2015-10-30 20:16:11 +0900

    ts-node로 TypeScript 코드 바로 실행하게 함.
    https://github.com/TypeStrong/ts-node

--- 20151015/test.js
+++
@@ -1,8 +0,0 @@
-// Generated JavaScript file. DO NOT EDIT!
-
-console.log(2 == 1 + 1);
-console.log('31' == '' + 3 + 1);
-var add = function (a, b) {
-    return a + b;
-};
-console.log(5 == add(2, 3));
