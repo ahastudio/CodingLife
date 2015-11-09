9e7c9548f2fef4f222561b571e29c992f4f0218c
Ashal aka JOKER <ahastudio@gmail.com>
2015-11-10 03:57:05 +0900

    Equal을 ===로 처리.

--- 20151015/test.ts
+++ 20151015/test.ts
@@ -1,17 +1,17 @@
-console.log(2 == 1 + 1);
+console.log(2 === 1 + 1);
 
 // 아래는 에러!
 // Operator '==' cannot be applied to types 'string' and 'number'.
-// console.log('2' == 1 + 1);
+// console.log('2' === 1 + 1);
 
-console.log('31' == '' + 3 + 1);
+console.log('31' === '' + 3 + 1);
 
 var add = (a, b): number => {
     return a + b;
 }
 
-console.log(5 == add(2, 3));
+console.log(5 === add(2, 3));
 
 // 아래는 에러!
 // Operator '==' cannot be applied to types 'string' and 'number'.
-// console.log('5' == add(2, 3));
+// console.log('5' === add(2, 3));
