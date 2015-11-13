4dfed8758a47708f44885aaacf8c33982392026a
Ashal aka JOKER <ahastudio@gmail.com>
2015-11-13 21:34:00 +0900

    간단한 머지소트 작성
    https://ko.wikipedia.org/wiki/%ED%95%A9%EB%B3%91_%EC%A0%95%EB%A0%AC

--- 20151112/clojure/test.clj
+++ 20151112/clojure/test.clj
@@ -6,13 +6,38 @@
 
 (defn qsort
   ([] [])
-  ([pivot & data]
-   (concat (apply qsort (filter (partial >= pivot) data))
+  ([pivot & v]
+   (concat (apply qsort (filter (partial >= pivot) v))
            [pivot]
-           (apply qsort (filter (partial < pivot) data)))))
+           (apply qsort (filter (partial < pivot) v)))))
 
 (qsort 1 2 3 4 5 6 7 8 9)
 (qsort 1 2 3 5 6 7 8 9 4)
 (qsort 9 8 7 6 5 4 3 2 1)
 (qsort 3 2 1 9 8 7 6 5 4)
 (qsort 1 2 1 9 2 1 3 3 9)
+
+; Merge Sort
+
+(defn msort [v]
+  (defn step [result left right]
+    (cond (empty? left) (concat result right)
+          (empty? right) (concat result left)
+          :else (let [a (first left)
+                      b (first right)]
+                  (if (< a b)
+                    (step (conj result a) (drop 1 left) right)
+                    (step (conj result b) left (drop 1 right))))))
+  (let [length (count v)]
+    (if (<= length 1)
+      v
+      (let [half (quot length 2)
+            left (msort (take half v))
+            right (msort (drop half v))]
+        (step [] left right)))))
+
+(msort [1 2 3 4 5 6 7 8 9])
+(msort [1 2 3 5 6 7 8 9 4])
+(msort [9 8 7 6 5 4 3 2 1])
+(msort [3 2 1 9 8 7 6 5 4])
+(msort [1 2 1 9 2 1 3 3 9])
