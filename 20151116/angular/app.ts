87ae1b4b5b31faa484ec2b1e23e8b39bed43017a
Ashal aka JOKER <ahastudio@gmail.com>
2015-11-16 22:57:51 +0900

    Angular 2 - 5 Min Quickstart
    Angular https://angular.io/
    
    5 Min Quickstart https://angular.io/docs/ts/latest/quickstart.html

---
+++ 20151116/angular/app.ts
@@ -0,0 +1,13 @@
+// https://angular.io/docs/ts/latest/quickstart.html
+
+import {bootstrap, Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello, Angular 2</h1>'
+})
+
+class AppComponent {
+}
+
+bootstrap(AppComponent);
