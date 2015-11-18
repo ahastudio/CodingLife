ddc3f002a50f102f34d74217472c32f4dd46742c
Ashal aka JOKER <ahastudio@gmail.com>
2015-11-18 09:25:18 +0900

    다중 통화 연산을 나중에 몰아서 수행하게 함.

--- 20151117/money/test.rb
+++ 20151117/money/test.rb
@@ -1,4 +1,45 @@
 class Money
+  attr_reader :units
+
+  def initialize(units)
+    fail unless units.all? { |i| i.is_a?(MoneyUnit) }
+    @units = units
+  end
+
+  def ==(other)
+    @units.size == other.units.size &&
+      @units.zip(other.units).all? { |a, b| a == b }
+  end
+
+  def *(times)
+    Money.new(@units.map { |i| i * times })
+  end
+
+  def +(other)
+    Money.new(@units.concat(other.units))
+  end
+
+  def exchange(currency)
+    reduced = @units.reduce(MoneyUnit.new(0, currency)) do |a, e|
+      a + e.exchange(currency)
+    end
+    Money.unit(reduced.amount, currency)
+  end
+
+  def self.unit(amount, currency)
+    Money.new([MoneyUnit.new(amount, currency)])
+  end
+
+  def self.dollar(amount)
+    Money.unit(amount, :USD)
+  end
+
+  def self.franc(amount)
+    Money.unit(amount, :CHF)
+  end
+end
+
+class MoneyUnit
   attr_reader :amount, :currency
 
   def initialize(amount, currency)
@@ -11,30 +52,21 @@ class Money
   end
 
   def *(times)
-    Money.new(@amount * times, @currency)
+    MoneyUnit.new(@amount * times, @currency)
   end
 
   def +(other)
-    if @currency == other.currency
-      Money.new(@amount + other.amount, @currency)
-    else
-      self + other.exchange(@currency)
-    end
+    fail unless @currency == other.currency
+    MoneyUnit.new(@amount + other.amount, @currency)
   end
 
   def exchange(currency)
     return self if @currency == currency
-    Money.new(@amount * Money.rate(@currency, currency), currency)
+    MoneyUnit.new(@amount * MoneyRate.rate(@currency, currency), currency)
   end
+end
 
-  def self.dollar(amount)
-    Money.new(amount, :USD)
-  end
-
-  def self.franc(amount)
-    Money.new(amount, :CHF)
-  end
-
+class MoneyRate
   def self.rate(from, to)
     {
       USD: { CHF: 2 },
@@ -46,13 +78,24 @@ end
 puts '---------------------------------------------'
 
 describe Money do
-  it 'multiply money and times' do
+  it 'multiplies money and times' do
     expect(Money.dollar(5) * 2).to eq(Money.dollar(10))
     expect(Money.franc(5) * 2).to eq(Money.franc(10))
   end
 
-  it 'add USD and CHF' do
-    expect(Money.dollar(5) + Money.franc(10)).to eq(Money.dollar(10))
-    expect(Money.franc(10) + Money.dollar(5)).to eq(Money.franc(20))
+  it 'addes money' do
+    expect((Money.dollar(5) + Money.dollar(10)).units.reduce(&:+))
+      .to eq(MoneyUnit.new(15, :USD))
+  end
+
+  it 'exchanges the money' do
+    expect(Money.franc(10).exchange(:USD)).to eq(Money.dollar(5))
+  end
+
+  it 'addes USD and CHF' do
+    expect((Money.dollar(5) + Money.franc(10)).exchange(:USD))
+      .to eq(Money.dollar(10))
+    expect((Money.franc(10) + Money.dollar(5)).exchange(:CHF))
+      .to eq(Money.franc(20))
   end
 end
