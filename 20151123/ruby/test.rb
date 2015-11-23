4d3c729f9126dbf19187dc5eebbaa40f58543c5c
Ashal aka JOKER <ahastudio@gmail.com>
2015-11-24 01:58:14 +0900

    Ruby Lambda 예제 추가

--- 20151123/ruby/test.rb
+++ 20151123/ruby/test.rb
@@ -1,5 +1,15 @@
-describe "Lambda" do
-  it "calls a lambda" do
+describe 'Lambda' do
+  it 'calls a lambda' do
     expect(-> x { x + 1 }[2]).to eq(3)
+  end
+
+  it 'returns a function' do
+    add = -> (a, b) { a + b }
+
+    increase = -> x {
+      -> i { add[i, x] }
+    }
+
+    expect(increase[1][2]).to eq(3)
   end
 end
