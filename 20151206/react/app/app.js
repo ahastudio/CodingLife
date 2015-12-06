86b0fa4c6babdb6628bb0a11226f7d947bb829df
Ashal aka JOKER <ahastudio@gmail.com>
2015-12-06 23:43:38 +0900

    렌더링되는 HTML을 여러 줄로 늘림
    Multiline일 땐 괄호로 싸준다.

--- 20151206/react/app/app.js
+++ 20151206/react/app/app.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 
 class Hello extends React.Component {
   render() {
-    return <h1>Hello, world!</h1>;
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>Hello, world!</p>
+      </div>
+    );
   }
 }
 
