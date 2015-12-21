9ba1baee19621d639e582566dff088de3ee19d76
Ashal aka JOKER <ahastudio@gmail.com>
2015-12-22 02:56:22 +0900

    Angular 2 beta 예제

---
+++ 20151221/angular/README.md
@@ -0,0 +1,64 @@
+## jspm 설치
+
+http://jspm.io/
+
+```
+$ npm install jspm -g
+```
+
+## NPM 패키지 설정
+
+```
+$ npm init -y
+$ vi package.json
+
+적절히 수정
+```
+
+## jspm 패키지 설정
+
+```
+$ npm install jspm --save-dev
+$ jspm init -y
+```
+
+## TypeScript 사용
+
+```
+$ jspm dl-loader typescript
+```
+
+## Angular 2 설치
+
+```
+$ jspm install angular2
+$ jspm install
+```
+
+## HTML 파일 작성
+
+```
+$ vi index.html
+```
+
+`index.html` 파일 참고
+
+## TS 파일 생성
+
+```
+$ mkdir app
+$ vi app/boot.ts
+```
+
+`boot.ts` 파일 참고
+
+## live-server 설치 및 실행
+
+https://github.com/tapio/live-server
+
+```
+$ npm install live-server -g
+$ live-server
+```
+
+http://localhost:8080/
---
+++ 20151221/angular/app/boot.ts
@@ -0,0 +1,47 @@
+import 'reflect-metadata'
+import {bootstrap} from 'angular2/platform/browser'
+import {Component} from 'angular2/core'
+
+@Component({
+  selector: 'my-app'
+  template: `
+    <form>
+      <h1>Hello, {{name}}!</h1>
+      <input [(ngModel)]="name" placeholder="Name">
+      <button (click)="onSubmit()">Go!</button>
+      <p>{{json}}</p>
+    </form>
+  `,
+  styles: [`
+    form {
+      width: 50%;
+      margin: 20px auto;
+    }
+    h1 {
+      margin: 0;
+      padding-bottom: 0.4em;
+      font-size: 2.0em;
+    }
+    input, button {
+      padding: 0.5em;
+      font-size: 1.0em;
+    }
+  `]
+})
+class AppComponent {
+  public name: string = 'Angular 2';
+
+  public count: number = 0;
+  public json: string = '';
+
+  onSubmit() {
+    this.json = JSON.stringify({
+      count: this.count += 1,
+      name: this.name,
+      datetime: new Date()
+    });
+    this.name = 'Angular 2';
+  }
+}
+
+bootstrap(AppComponent);
