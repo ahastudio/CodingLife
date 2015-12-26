8776c58d355a73ae8f254f07f068753bf7cf83ed
Ashal aka JOKER <ahastudio@gmail.com>
2015-12-27 05:03:07 +0900

    80 컬럼 제한
    https://github.com/dalzony/ruby-style-guide/blob/master/README-koKR.md#80-character-limits

--- 20151222/rails-5.0.0.beta1-demo/app/controllers/comments_controller.rb
+++ 20151222/rails-5.0.0.beta1-demo/app/controllers/comments_controller.rb
@@ -29,11 +29,15 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html {
+          redirect_to @comment, notice: 'Comment was successfully created.'
+        }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.json {
+          render json: @comment.errors, status: :unprocessable_entity
+        }
       end
     end
   end
@@ -43,11 +47,15 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html {
+          redirect_to @comment, notice: 'Comment was successfully updated.'
+        }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.json {
+          render json: @comment.errors, status: :unprocessable_entity
+        }
       end
     end
   end
@@ -57,7 +65,9 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html {
+        redirect_to comments_url, notice: 'Comment was successfully destroyed.'
+      }
       format.json { head :no_content }
     end
   end
@@ -69,7 +79,8 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+  # Never trust parameters from the scary internet,
+  # only allow the white list through.
   def comment_params
     params.require(:comment).permit(:body)
   end
