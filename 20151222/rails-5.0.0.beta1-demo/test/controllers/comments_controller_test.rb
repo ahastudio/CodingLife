3827280f506bfbfd8c37dacc732913433a837683
Ashal aka JOKER <ahastudio@gmail.com>
2015-12-27 04:53:49 +0900

    문자열을 작은 따옴표로 통일
    https://github.com/dalzony/ruby-style-guide/blob/master/README-koKR.md#consistent-string-literals

--- 20151222/rails-5.0.0.beta1-demo/test/controllers/comments_controller_test.rb
+++ 20151222/rails-5.0.0.beta1-demo/test/controllers/comments_controller_test.rb
@@ -6,17 +6,17 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     @comment = comments(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get comments_url(post_id: @post)
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_comment_url(post_id: @post)
     assert_response :success
   end
 
-  test "should create comment" do
+  test 'should create comment' do
     assert_difference('Comment.count') do
       post comments_url(post_id: @post), params: {
         comment: { body: @comment.body }
@@ -26,24 +26,24 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to comment_path(Comment.last)
   end
 
-  test "should show comment" do
+  test 'should show comment' do
     get comment_url(@comment)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_comment_url(@comment)
     assert_response :success
   end
 
-  test "should update comment" do
+  test 'should update comment' do
     patch comment_url(@comment), params: {
       comment: { body: @comment.body }
     }
     assert_redirected_to comment_path(@comment)
   end
 
-  test "should destroy comment" do
+  test 'should destroy comment' do
     assert_difference('Comment.count', -1) do
       delete comment_url(@comment)
     end
