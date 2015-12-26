994259c1265c4caeb3e02214b8c55b7e0af74d4a
Ashal aka JOKER <ahastudio@gmail.com>
2015-12-27 04:57:27 +0900

    private 처리 변경
    1. 메서드 들여쓰기 일정하게 함.
    2. private 다음 줄 비움.
    
    https://github.com/dalzony/ruby-style-guide/blob/master/README-koKR.md#indent-public-private-protected

--- 20151222/rails-5.0.0.beta1-demo/app/controllers/comments_controller.rb
+++ 20151222/rails-5.0.0.beta1-demo/app/controllers/comments_controller.rb
@@ -63,13 +63,14 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
