d5b6e23223bb7bbe7bf54b7b9be7c594183363f4
Ashal aka JOKER <ahastudio@gmail.com>
2016-03-18 21:42:21 +0900

    라이브코딩 결과

--- 20160318/tdd/test.py
+++ 20160318/tdd/test.py
@@ -2,26 +2,23 @@ import unittest
 
 
 def multiply(x, y):
-    if y == 0:
-        return 0
     if y < 0:
         x, y = -x, -y
+    if y == 0:
+        return 0
     if y % 2 == 0:
         return multiply(x + x, y / 2)
     return x + multiply(x, y - 1)
-    # return reduce(lambda a, e: a + x, range(y), 0)
+    # return reduce(lambda a, b: a + x, range(y), 0)
 
 
 class MultiplicationTest(unittest.TestCase):
     def test_simple(self):
         self.assertEqual(6, multiply(3, 2))
+        self.assertEqual(9, multiply(3, 3))
         self.assertEqual(-6, multiply(3, -2))
-        self.assertEqual(-6, multiply(-3, 2))
-        self.assertEqual(6, multiply(-3, -2))
-
-    def test_large_case(self):
-        self.assertEqual(37 * 99, multiply(37, 99))
-        self.assertEqual(37 * 100, multiply(37, 100))
+        self.assertEqual(0, multiply(3, 0))
+        self.assertEqual(300, multiply(3, 100))
 
 
 if __name__ == '__main__':
