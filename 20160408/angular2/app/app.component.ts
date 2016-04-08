8ced390534b15dd37a988955ae3d04687cdbd5b2
Ashal aka JOKER <ahastudio@gmail.com>
2016-04-08 19:04:03 +0900

    아이템 목록 및 상세 보기 추가
    Master/Detail - ts
    https://angular.io/docs/ts/latest/tutorial/toh-pt2.html

--- 20160408/angular2/app/app.component.ts
+++ 20160408/angular2/app/app.component.ts
@@ -1,8 +1,42 @@
 import {Component} from 'angular2/core';
 
+class Item {
+  id: number;
+  name: string;
+}
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>My Application</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>My Application</h1>
+    <ul *ngIf="items.length">
+      <li *ngFor="#item of items">
+        <a href="#" (click)="onClick(item)">{{item.name}}</a>
+      </li>
+    </ul>
+    <div *ngIf="item">
+      <h2>Detail</h2>
+      <p>{{item.name}}</p>
+    </div>
+  `
 })
 export class AppComponent {
+  items: Item[] = [];
+  item: Item = null;
+
+  constructor() {
+    this.items = [
+      {id: 1, name: 'A'},
+      {id: 2, name: 'B'},
+    ];
+  }
+
+  onClick(item: Item) {
+    if (item == this.item) {
+      this.item = null;
+      return false;
+    }
+    this.item = item;
+    return false;
+  }
 }
