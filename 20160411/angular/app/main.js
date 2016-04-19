b0ce2969eb1e2a6269a84b67e866058d9f1aab15
Ashal aka JOKER <ahastudio@gmail.com>
2016-04-19 20:53:26 +0900

    Typings 사용
    Typings - The TypeScript Definition Manager
    - https://www.npmjs.com/package/typings
    - https://github.com/typings/typings

--- 20160411/angular/app/main.js
+++ 20160411/angular/app/main.js
@@ -1,4 +1,3 @@
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
 System.register(['angular2/platform/browser', 'angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
