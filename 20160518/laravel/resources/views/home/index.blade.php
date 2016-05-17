78c7fdf3c33d56f0a2da776d95cdaeb38d8a83d2
Ashal aka JOKER <ahastudio@gmail.com>
2016-05-18 06:17:17 +0900

    HomeController 추가

---
+++ 20160518/laravel/resources/views/home/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laravel Sample</title>
+</head>
+<body>
+    <h1>Laravel 5.2 Sample</h1>
+    <p>Welcome!</p>
+</body>
+</html>
