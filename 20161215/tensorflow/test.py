8728c6ce101e47ddb5156fcccfa218bee6dd09f9
Ashal aka JOKER <ahastudio@gmail.com>
2016-12-16 07:51:08 +0900

    단일 퍼셉트론으로 손글씨 인식 처리
    단일 퍼셉트론으로 손글씨 인식 처리.
    
    MNIST 데이터베이스를 이용해 숫자를 인식할 수 있게 함.
    
    경사하강 등 복잡한 부분은 TensorFlow를 이용해 간단히 처리함.

---
+++ 20161215/tensorflow/test.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+
+
+def main():
+    mnist = read_data_sets('/tmp/tensorflow/mnist/input_data', one_hot=True)
+
+    n = 28 * 28
+    k = 10
+    rate = 0.5
+
+    w = tf.Variable(tf.zeros((n, k)))
+    b = tf.Variable(tf.zeros((k)))
+
+    x = tf.placeholder(tf.float32, (None, n))
+    y = tf.placeholder(tf.float32, (None, k))
+
+    u = tf.matmul(x, w) + b
+    z = tf.nn.softmax(u)
+
+    cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(z), 1))
+    optimizer = tf.train.GradientDescentOptimizer(rate).minimize(cost)
+
+    session = tf.Session()
+    session.run(tf.initialize_all_variables())
+
+    for i in range(1000):
+        x_data, y_data = mnist.train.next_batch(100)
+        session.run(optimizer, {x: x_data, y: y_data})
+
+    #
+
+    x_data, y_data = mnist.test.images, mnist.test.labels
+    correct = tf.equal(tf.argmax(y, 1), tf.argmax(z, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    print session.run(accuracy, {x: x_data, y: y_data})
+
+    #
+
+    def draw(image):
+        print '--' * 28
+        for i in range(28):
+            offset = i * 28
+            print ''.join(['%2d' % (j * 10) for j in image[offset:offset + 28]])
+
+    z_data = session.run(tf.argmax(z, 1), {x: x_data, y: y_data})
+    corrects = session.run(correct, {x: x_data, y: y_data})
+
+    count = 3
+    for i in range(len(corrects)):
+        if corrects[i]:
+            continue
+        draw(x_data[i])
+        print 'right: %d / predict: %d' % (list(y_data[i]).index(1), z_data[i])
+        count -= 1
+        if count is 0:
+            break
+
+
+if __name__ == '__main__':
+    main()
