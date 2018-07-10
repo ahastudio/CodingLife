1009b1fc54d22004f5d75f3bcf361edce18be1aa
Ashal aka JOKER <ahastudio@gmail.com>
2018-07-11 03:07:57 +0900

    Self Number
    Coding Dojo Problem 1
    
    https://www.facebook.com/Coding-Dojo-175800442453639/
    
    https://github.com/seattlerb/minitest
    
    https://github.com/guard/guard-minitest
    
    https://github.com/yujinakayama/guard-rubocop
    
    https://en.wikipedia.org/wiki/Monkey_patch
    
    https://en.wikipedia.org/wiki/Self_number

---
+++ 20180710/ruby/README.md
@@ -0,0 +1,9 @@
+# Self Number
+
+Problem: http://j.mp/2m7AFG3
+
+```
+$ bundle
+$ bundle exec rubocop
+$ bundle exec guard
+```
---
+++ 20180710/ruby/test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+# Monkey patch
+class Integer
+  def digits
+    to_s.split('').map(&:to_i)
+  end
+
+  def d
+    digits.sum + self
+  end
+end
+
+def self_numbers(max)
+  [*1..max] - (1..max).map(&:d)
+end
+
+# Test ----------------------------------------------------------
+class SelfNumberTest < MiniTest::Unit::TestCase
+  def test_function_d
+    assert_equal 9 + 1 + 91, 91.d
+    assert_equal 1 + 0 + 100, 100.d
+  end
+
+  def test_self_numbers
+    assert_equal [1, 3, 5, 7, 9, 20, 31], self_numbers(31)
+  end
+
+  def test_solve
+    assert_equal 1_227_365, self_numbers(5000 + 1).sum
+  end
+end
