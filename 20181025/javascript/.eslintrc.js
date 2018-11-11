f930e691ca5bf04148e86e26b1cb450f90a97597
Ashal aka JOKER <ahastudio@gmail.com>
2018-11-11 22:29:41 +0900

    Separate module and test code
    Extract codes to a new module file.

--- 20181025/javascript/.eslintrc.js
+++ 20181025/javascript/.eslintrc.js
@@ -1,28 +1,20 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2016
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    'ecmaVersion': 2016,
+  },
+  rules: {
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [ 'error', 'unix' ],
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'always' ],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
 };
--- 20181025/javascript/spiral.test.js
+++ 20181025/javascript/spiral.test.js
@@ -1,42 +1,9 @@
-/* global describe it expect */
-
-const _ = require('lodash');
-
-const spiralMatrix = (n) => {
-  const m = [...Array(n)].map(() => [...Array(n)]);
-  spiralPoints(n).forEach(([x, y], i) => m[y][x] = i);
-  return m;
-
-  // OR
-
-  // const points = spiralPoints(n);
-  // return _.range(n).map(y => _.range(n).map(x =>
-  //   points.findIndex(_.matches([x, y]))
-  // ));
-};
-
-const initialState = {
-  x: 0,
-  y: 0,
-  dx: 1,
-  dy: 0,
-  points: [],
-};
-
-const spiralPoints = (n) =>
-  _.range(n * n).reduce(state => movePoint(n, state), initialState).points;
-
-const movePoint = (n, state) => {
-  const { dx, dy } = delta(n, state);
-  const { x, y, points } = state;
-  return { x: x + dx, y: y + dy, dx, dy, points: [...points, [x, y]] };
-};
-
-const delta = (n, { x, y, dx, dy, points }) =>
-  checkPoint(n, x + dx, y + dy, points) ? { dx, dy } : { dx: -dy, dy: dx };
-
-const checkPoint = (n, x, y, points) =>
-  _.inRange(x, n) && _.inRange(y, n) && !points.find(_.matches([x, y]));
+const {
+  spiralMatrix,
+  spiralPoints,
+  movePoint,
+  checkPoint,
+} = require('./spiral');
 
 describe('spiralMatrix', () => {
   describe('when n = 1', () => {
