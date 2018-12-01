d9fe88fc256f30d4f72f87fcf4c542b6f7cb35a1
Ashal aka JOKER <ahastudio@gmail.com>
2018-12-02 00:51:43 +0900

    jQuery 의존성 제거
    jQuery 의존성을 제거하기 위해 axios와 FullCalendar v4 사용.

--- 20151201/calendar/js/application.js
+++ 20151201/calendar/js/application.js
@@ -1,34 +1,38 @@
 var id = '1n1_O6XJUOCdzZ1NK2Lld3aTOATb3IsLmyHrz-eXKzJQ';
 var url = 'https://spreadsheets.google.com/feeds/list/' + id + '/od6/public/values?alt=json';
 
-$(function() {
-  $.getJSON(url, function (json) {
-    var events = [];
-    json.feed.entry.each(function(row) {
-      var date = moment(row['gsx$date'].$t)
-      if (date) {
-        var author = row['gsx$author'].$t;
-        var title = row['gsx$title'].$t;
-        var url = row['gsx$url'].$t;
-        if (title) {
-          events.push({
-            title: '[' + author + '] ' + title,
-            start: date.format('YYYY-MM-DD'),
-            url: url
-          });
-        }
+axios.get(url)
+  .then(function(response) {
+    var items = response.data.feed.entry;
+    var events = items.map(function(row) {
+      var date = moment(row['gsx$date'].$t, 'YYYY. MM. DD');
+      var author = row['gsx$author'].$t;
+      var title = row['gsx$title'].$t;
+      var url = row['gsx$url'].$t;
+      if (!date.isValid() || !title) {
+        return null;
       }
-    });
+      url = url.match(/^http/) ? url : '';
+      return {
+        start: date.format('YYYY-MM-DD'),
+        title: '[' + author + '] ' + title + (url ? '' : ' [예약]'),
+        url: url
+      };
+    }).filter(function(event) { return event != null; });
 
-    $('#calendar').fullCalendar({
+    var element = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(element, {
       defaultDate: '2015-12-25',
       events: events,
-      eventClick: function(event) {
-        if (event.url && event.url.match(/^http/)) {
-          window.open(event.url, '_blank');
+      eventClick: function(info) {
+        info.jsEvent.preventDefault();
+        if (info.event.url) {
+          window.open(info.event.url);
         }
-        return false;
       }
     });
+    calendar.render();
+  })
+  .catch(function(error) {
+    alert(error);
   });
-});
