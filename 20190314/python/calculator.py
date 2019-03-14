bda8332b25733c6bc7189746f43aa3b274bc41fc
Ashal aka JOKER <ahastudio@gmail.com>
2019-03-14 19:40:57 +0900

    버튼 만들기 일반화

--- 20190314/python/calculator.py
+++ 20190314/python/calculator.py
@@ -1,5 +1,18 @@
 import tkinter as tk
 
+NUMBER_SYMBOLS = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    ['0', '.', '=']
+]
+
+OPERATOR_SYMBOLS = [
+    ['*', '/'],
+    ['+', '-'],
+    ['(', ')'],
+    ['C', 'AC']
+]
 
 # 참고: https://docs.python.org/3/library/tkinter.html
 class Application(tk.Frame):
@@ -10,50 +23,24 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         self.create_display()
-        self.create_number_buttons()
-        self.create_operator_buttons()
+        self.create_buttons(0, NUMBER_SYMBOLS)
+        self.create_buttons(1, OPERATOR_SYMBOLS)
 
     def create_display(self):
         self.display = tk.Entry(self, width=35, bg='light green')
         self.display.grid(row=0, column=0, columnspan=2)
 
-    def create_number_buttons(self):
-        num_frame = tk.Frame(self)
-        num_frame.grid(row=1, column=0)
-
-        symbols = [
-            '7', '8', '9',
-            '4', '5', '6',
-            '1', '2', '3',
-            '0', '.', '='
-        ]
-        columns = 3
-
-        for index, symbol in enumerate(symbols):
-            button = self.create_button(num_frame, symbol)
-            button.grid(row=index // columns, column=index % columns)
-
-    def create_operator_buttons(self):
-        op_frame = tk.Frame(self)
-        op_frame.grid(row = 1, column = 1)
-
-        symbols = [
-            '*', '/',
-            '+', '-',
-            '(', ')',
-            'C', 'AC'
-        ]
-        columns = 2
-
-        for index, symbol in enumerate(symbols):
-            button = self.create_button(op_frame, symbol)
-            button.grid(row=index // columns, column=index % columns)
+    def create_buttons(self, column, symbols):
+        frame = tk.Frame(self)
+        frame.grid(row=1, column=column)
+        for row, row_symbols in enumerate(symbols):
+            for column, symbol in enumerate(row_symbols):
+                button = self.create_button(frame, symbol)
+                button.grid(row=row, column=column)
 
     def create_button(self, frame, symbol):
-        def handler():
-            return self.click(symbol)
-
-        return tk.Button(frame, text=symbol, width=5, command=handler)
+        return tk.Button(frame, text=symbol, width=5,
+                         command=lambda: self.click(symbol))
 
     def click(self, text):
         if text == "=":
