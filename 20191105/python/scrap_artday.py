d684f61cb8f1f41dd1e810067e969daacd7d9dab
Ashal aka JOKER <ahastudio@gmail.com>
2019-11-06 07:51:43 +0900

    아트데이옥션 온라인미술품 경매

---
+++ 20191105/python/scrap_artday.py
@@ -0,0 +1,31 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+class ArtdayAuctionSpider(scrapy.Spider):
+    name = 'artday-auction'
+    start_urls = [
+        f'http://www.artday.co.kr/pages/auction/online-auction.php?page={i}'
+        for i in range(1, 30)
+    ]
+
+    def parse(self, response):
+        for item in response.css('.auction-table tbody tr'):
+            image = item.css('td:nth-child(2) img::attr(src)').get()
+            yield {
+                'image': f'http://www.artday.co.kr{image}',
+                'artist': item.css('td:nth-child(3) .list-first::text').get(),
+                'title': item.css('td:nth-child(4) .list-first::text').get(),
+                'price': item.css('td:nth-child(5) .price::text').get(),
+            }
+
+
+if __name__ == '__main__':
+    process = CrawlerProcess({
+        **get_project_settings(),
+        'FEED_FORMAT': 'CSV',
+        'FEED_URI': 'output/items.csv'
+    })
+    process.crawl(ArtdayAuctionSpider)
+    process.start()
