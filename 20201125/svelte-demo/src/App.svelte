a27794b540bd007d1639c439f11965e57edd2f3d
Ashal aka JOKER <ahastudio@gmail.com>
2020-11-26 00:45:22 +0900

    Svelte demo
    https://www.youtube.com/watch?v=-n-MHYJvh_I

---
+++ 20201125/svelte-demo/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Page from './Page.svelte';
+
+	export let title;
+
+	let name = '아샬';
+
+	function handleChangeName(event) {
+		const { value } = event.target;
+		name = value;
+	}
+</script>
+
+<div>
+	<h1>{title}</h1>
+	<hr />
+	<Page {name} onChangeName={handleChangeName} />
+</div>
+
+<style>
+</style>
---
+++ 20201125/svelte-demo/src/Page.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let name;
+	export let onChangeName;
+</script>
+
+<main>
+	<h2>Hello {name}!</h2>
+	<!-- <input type="text" bind:value={name} /> -->
+	<input type="text" value={name} on:input={onChangeName} />
+	<p>
+		Visit the
+		<a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h2 {
+		color: #ff3e00;
+		/* text-transform: uppercase; */
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
