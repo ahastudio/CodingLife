911a2e805394d8ea7472ace848aa862299f58cdf
Ashal aka JOKER <ahastudio@gmail.com>
2020-12-12 13:48:29 +0900

    Gradle JAR example

---
+++ 20201212/java/app/src/main/java/com/example/App.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(App.class);
+
+        App app = context.getBean("myApp", App.class);
+
+        System.out.println(app.getGreeting());
+    }
+
+    @Bean
+    public App myApp() {
+        return new App();
+    }
+}
