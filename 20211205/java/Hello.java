ed3cb4d3cd3e9fd3352975e019dc4bbaa30ae7da
Ashal aka JOKER <ahastudio@gmail.com>
2021-12-08 16:29:11 +0900

    String.formatted
    https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/String.html

--- 20211205/java/Hello.java
+++ 20211205/java/Hello.java
@@ -62,12 +62,15 @@ class Hello {
 
         for (int i = 0; i < 3; i++) {
             answers[i] = (int)(Math.random() * 10);
-            System.out.println((i + 1) + " - " + answers[i]);
         }
+
+        System.out.println(
+                "[%d %d %d]".formatted(answers[0], answers[1], answers[2]));
 
         int[] inputs = new int[3];
 
         while (true) {
+            System.out.println();
             System.out.println("Guess numbers!");
 
             for (int i = 0; i < 3; i++) {
@@ -80,24 +83,29 @@ class Hello {
             for (int i = 0; i < 3; i++) {
               for (int j = 0; j < 3; j++) {
                 if (inputs[i] == answers[j]) {
-                  if (i == j) {
-                    strike += 1;
-                    continue;
-                  }
-                  ball += 1;
+                    if (i == j) {
+                        strike += 1;
+                        continue;
+                    }
+                    ball += 1;
                 }
               }
             }
 
+            System.out.println();
+
             if (strike > 0) {
                 System.out.print(strike + " strike ");
             }
+
             if (ball > 0) {
                 System.out.print(ball + " ball ");
             }
+
             System.out.println();
 
             if (strike == 3) {
+                System.out.println();
                 System.out.println("Yes, you got it!");
                 break;
             }
