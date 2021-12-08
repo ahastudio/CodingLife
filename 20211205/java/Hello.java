c545f2fc51c9d83c08f6478ec8d6362d8e6d245c
Ashal aka JOKER <ahastudio@gmail.com>
2021-12-08 15:33:30 +0900

    Baseball Game

--- 20211205/java/Hello.java
+++ 20211205/java/Hello.java
@@ -50,8 +50,59 @@ class Hello {
         Scanner scanner = new Scanner(System.in);
 
         String name = scanner.nextLine();
-        scanner.close();
 
         System.out.println("Hello, " + name + "!");
+
+        // Baseball Game
+        // https://www.hackster.io/hyun-woo-park/baseball-game-daecdd
+
+        System.out.println("-".repeat(80));
+
+        int[] answers = new int[]{ 0, 0, 0 };
+
+        for (int i = 0; i < 3; i++) {
+            answers[i] = (int)(Math.random() * 10);
+            System.out.println((i + 1) + " - " + answers[i]);
+        }
+
+        int[] inputs = new int[3];
+
+        while (true) {
+            System.out.println("Guess numbers!");
+
+            for (int i = 0; i < 3; i++) {
+                inputs[i] = scanner.nextInt();
+            }
+
+            int strike = 0;
+            int ball = 0;
+
+            for (int i = 0; i < 3; i++) {
+              for (int j = 0; j < 3; j++) {
+                if (inputs[i] == answers[j]) {
+                  if (i == j) {
+                    strike += 1;
+                    continue;
+                  }
+                  ball += 1;
+                }
+              }
+            }
+
+            if (strike > 0) {
+                System.out.print(strike + " strike ");
+            }
+            if (ball > 0) {
+                System.out.print(ball + " ball ");
+            }
+            System.out.println();
+
+            if (strike == 3) {
+                System.out.println("Yes, you got it!");
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
