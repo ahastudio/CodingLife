53904c65fcb68b22b92f419e7cf57fc99bcfbade
Ashal aka JOKER <ahastudio@gmail.com>
2021-12-08 18:00:37 +0900

    Use List instead of array

--- 20211205/java/Baseball.java
+++ 20211205/java/Baseball.java
@@ -1,6 +1,10 @@
 // Baseball Game
 // https://www.hackster.io/hyun-woo-park/baseball-game-daecdd
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 class Baseball {
@@ -9,30 +13,23 @@ class Baseball {
 
         // Shuffle
 
-        int[] numbers = new int[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-        for (int i = 0; i < 100; i++) {
-            int a = (int)(Math.random() * 10);
-            int b = (int)(Math.random() * 10);
-            int temp = numbers[a];
-            numbers[a] = numbers[b];
-            numbers[b] = temp;
-        }
+        List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Collections.shuffle(numbers);
 
         // Generate answers
 
-        int[] answers = new int[3];
+        List<Integer> answers = new ArrayList<>();
 
         for (int i = 0; i < 3; i++) {
-            answers[i] = numbers[i];
+            answers.add(numbers.get(i));
         }
 
-        System.out.println(
-                "[%d %d %d]".formatted(answers[0], answers[1], answers[2]));
+        System.out.println("[%d %d %d]".formatted(
+                answers.get(0), answers.get(1), answers.get(2)));
 
         // Prepare inputs
 
-        int[] inputs = new int[3];
+        List<Integer> inputs = new ArrayList<>();
 
         while (true) {
             // Input numbers
@@ -41,8 +38,10 @@ class Baseball {
 
             System.out.println("Guess numbers!");
 
+            inputs.clear();
             for (int i = 0; i < 3; i++) {
-                inputs[i] = scanner.nextInt();
+                int number = scanner.nextInt();
+                inputs.add(number);
             }
 
             // Count strike and ball
@@ -52,7 +51,7 @@ class Baseball {
 
             for (int i = 0; i < 3; i++) {
               for (int j = 0; j < 3; j++) {
-                if (inputs[i] == answers[j]) {
+                if (inputs.get(i) == answers.get(j)) {
                     if (i == j) {
                         strike += 1;
                         continue;
