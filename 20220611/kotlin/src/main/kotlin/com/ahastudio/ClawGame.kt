04770c02ed4c528d06a7bbdf8b7f527d208da5bc
Ashal aka JOKER <ahastudio@gmail.com>
2022-06-18 22:04:02 +0900

    Claw Game 구현

--- 20220611/kotlin/src/main/kotlin/com/ahastudio/ClawGame.kt
+++ 20220611/kotlin/src/main/kotlin/com/ahastudio/ClawGame.kt
@@ -10,11 +10,57 @@ fun main() {
     )
 
     while (true) {
-        // TODO: board를 화면에 그려준다.
+        printBoard(board)
 
-        print("Input column number (1~5):")
-        val position = readLine()!!.toInt()
+        println()
 
-        // TODO: 입력 받은 위치의 상품을 꺼내고, 결과를 보여준다.
+        val position = inputPosition()
+
+        println()
+
+        val gift = pick(board, position)
+
+        printGift(gift)
+
+        println()
+    }
+}
+
+fun printBoard(board: Array<Array<String>>) {
+    for (row in board) {
+        print("|")
+        for (column in row) {
+            print(column)
+        }
+        println("|")
+    }
+    println("+" + "-".repeat(board[0].size) + "+")
+}
+
+fun inputPosition(): Int {
+    print("Input column number (1~5): ")
+    return readLine()!!.toInt()
+}
+
+fun pick(board: Array<Array<String>>, position: Int): String {
+    val index = position - 1
+    if (index !in board.indices) {
+        return ""
+    }
+
+    for (row in board) {
+        val column = row[index]
+        if (column.isNotBlank()) {
+            row[index] = " "
+            return column
+        }
+    }
+    return ""
+}
+
+fun printGift(gift: String) {
+    when {
+        gift.isNotBlank() -> println("You got \"$gift\".")
+        else -> println("There is nothing!")
     }
 }
