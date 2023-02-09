985503da94859e23f1cdd380d94760d2e61ee782
Ashal aka JOKER <ahastudio@gmail.com>
2023-02-09 17:36:41 +0900

    styled-components

--- 20230208/react/src/components/Greeting.tsx
+++ 20230208/react/src/components/Greeting.tsx
@@ -1,11 +1,37 @@
+import styled from 'styled-components';
+
+const Paragraph = styled.p`
+  color: #00F;
+
+  strong {
+    font-size: 2em;
+    color: #F00;
+  }
+`;
+
+const BigParagraph = styled(Paragraph)`
+  font-size: 2em;
+
+  strong {
+    font-size: 1.5em;
+  }
+`;
+
+function HelloWorld({ className }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <BigParagraph className={className}>
+      Hello, world
+      <strong>!</strong>
+    </BigParagraph>
+  );
+}
+
+const SmallHelloWorld = styled(HelloWorld)`
+  font-size: .1em;
+`;
+
 export default function Greeting() {
   return (
-    <p
-      style={{
-        color: '#00F',
-      }}
-    >
-      Hello, world!
-    </p>
+    <SmallHelloWorld />
   );
 }
