3f2604eb24c5dedfbe379d3e3e777d77caf5f1b6
Ashal aka JOKER <ahastudio@gmail.com>
2023-03-30 16:09:59 +0900

    Hibernate 6.2로 record 써보기

--- 20221213/spring/src/main/java/com/ahastudio/demo/shop/models/ProductId.java
+++ 20221213/spring/src/main/java/com/ahastudio/demo/shop/models/ProductId.java
@@ -2,20 +2,9 @@ package com.ahastudio.demo.shop.models;
 
 import jakarta.persistence.Column;
 
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode
-public class ProductId {
-    @Column(name = "id")
-    private String value;
-
-    private ProductId() {
-    }
-
-    public ProductId(String value) {
-        this.value = value;
-    }
-
+public record ProductId(
+        @Column(name = "id") String value
+) {
     @Override
     public String toString() {
         return String.valueOf(value);
--- 20221213/spring/src/main/java/com/ahastudio/demo/shop/models/ShopId.java
+++ 20221213/spring/src/main/java/com/ahastudio/demo/shop/models/ShopId.java
@@ -2,20 +2,9 @@ package com.ahastudio.demo.shop.models;
 
 import jakarta.persistence.Column;
 
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode
-public class ShopId {
-    @Column(name = "id")
-    private String value;
-
-    private ShopId() {
-    }
-
-    public ShopId(String value) {
-        this.value = value;
-    }
-
+public record ShopId(
+        @Column(name = "id") String value
+) {
     @Override
     public String toString() {
         return String.valueOf(value);
