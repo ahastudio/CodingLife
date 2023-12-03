a291ea1efa6b84aeb839be4ac59c40158c84666a
Ashal aka JOKER <ahastudio@gmail.com>
2023-12-03 11:49:48 +0900

    2023-11-26

--- 20231119/kotlin/src/test/kotlin/com/example/chapter05/Chapter05Test.kt
+++ 20231119/kotlin/src/test/kotlin/com/example/chapter05/Chapter05Test.kt
@@ -1,17 +1,26 @@
 package com.example.chapter05
 
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 import org.junit.jupiter.api.Test
+import com.example.chapter04.None
+import com.example.chapter04.Option
+import com.example.chapter04.Some
 
 sealed class Stream<out A> {
     companion object {
+        fun <A> cons(head: () -> A, tail: () -> Stream<A>): Stream<A> {
+            val h: A by lazy(head)
+            val t: Stream<A> by lazy(tail)
+            return Cons({ h }, { t })
+        }
+
         fun <A> of(vararg xs: A): Stream<A> = when {
             xs.isEmpty() -> Empty
             else -> Cons(
                 { xs[0] },
-                of(*xs.sliceArray(1 until xs.size))
+                { of(*xs.sliceArray(1 until xs.size)) }
             )
         }
     }
@@ -21,39 +30,85 @@ object Empty : Stream<Nothing>()
 
 data class Cons<out A>(
     val head: () -> A,
-    val tail: Stream<A>
+    val tail: () -> Stream<A>
 ) : Stream<A>()
 
 fun <A> Stream<A>.toList(): List<A> = when (this) {
     is Empty -> listOf()
-    is Cons -> listOf(this.head()) + this.tail.toList()
+    is Cons -> listOf(this.head()) + this.tail().toList()
+}
+
+fun <A> Stream<A>.take(n: Int): Stream<A> = when (this) {
+    is Empty -> Empty
+    is Cons ->
+        if (n == 0) Empty
+        else Cons(this.head) { this.tail().take(n - 1) }
 }
 
 fun <A> Stream<A>.drop(n: Int): Stream<A> = when (this) {
     is Empty -> Empty
     is Cons ->
         if (n == 0) this
-        else this.tail.drop(n - 1)
+        else this.tail().drop(n - 1)
 }
 
 fun <A, B> Stream<A>.foldRight(z: () -> B, f: (A, () -> B) -> B): B =
     when (this) {
         is Empty -> z()
-        is Cons -> f(this.head()) { this.tail.foldRight(z, f) }
+        is Cons -> f(this.head()) { this.tail().foldRight(z, f) }
     }
 
 //fun <A> Stream<A>.exists(p: (A) -> Boolean): Boolean = when (this) {
 //    is Empty -> false
-//    is Cons -> p(this.head()) || this.tail.exists(p)
+//    is Cons -> p(this.head()) || this.tail().exists(p)
 //}
 
 fun <A> Stream<A>.exists(p: (A) -> Boolean): Boolean =
     this.foldRight({ false }) { a: A, b: () -> Boolean -> p(a) || b() }
 
+fun <A> Stream<A>.forAll(p: (A) -> Boolean): Boolean =
+    this.foldRight({ true }) { a: A, b: () -> Boolean -> p(a) && b() }
+
+fun <A, B> Stream<A>.map(f: (A) -> B): Stream<B> =
+    this.foldRight({ Empty }) { cur: A, acc: () -> Stream<B> ->
+        Cons({ f(cur) }, acc)
+    }
+
+fun <A> Stream<A>.filter(p: (A) -> Boolean): Stream<A> =
+    this.foldRight({ Empty }) { cur: A, acc: () -> Stream<A> ->
+        if (p(cur)) Cons({ cur }, acc)
+        else acc()
+    }
+
+fun ones(): Stream<Int> = Cons({ 1 }, { ones() })
+
+fun <A, S> unfold(z: S, f: (S) -> Option<Pair<A, S>>): Stream<A> =
+    when (val x = f(z)) {
+        is None -> Empty
+        is Some -> {
+            val (a, b) = x.get
+            Cons({ a }, { unfold(b, f) })
+        }
+    }
+
+// 패턴 매칭이 된다면...
+// match (f(z)) {
+//     None -> Empty
+//     Some(a, b) -> Cons({ a }, { unfold(b, f) })
+// }
+
 class Chapter05Test {
     @Test
     fun testToList() {
         assertEquals(listOf(1, 2, 3), Stream.of(1, 2, 3).toList())
+    }
+
+    @Test
+    fun testTake() {
+        assertEquals(listOf(), Stream.of(1, 2, 3).take(0).toList())
+        assertEquals(listOf(1), Stream.of(1, 2, 3).take(1).toList())
+        assertEquals(listOf(1, 2), Stream.of(1, 2, 3).take(2).toList())
+        assertEquals(listOf(1, 2, 3), Stream.of(1, 2, 3).take(3).toList())
     }
 
     @Test
@@ -76,6 +131,85 @@ class Chapter05Test {
         assertEquals(
             6,
             Stream.of(1, 2, 3).foldRight({ 0 }) { cur, acc -> cur + acc() }
+        )
+    }
+
+    @Test
+    fun testForAll() {
+        assertTrue(Stream.of(1, 2, 3).forAll { x -> x > 0 })
+        assertFalse(Stream.of(1, 2, 3).forAll { x -> x > 1 })
+        assertFalse(Stream.of(1, 2, 3).forAll { x -> x % 2 == 1 })
+    }
+
+    @Test
+    fun testMap() {
+        assertEquals(
+            listOf(2, 4, 6),
+            Stream.of(1, 2, 3).map { x -> x * 2 }.toList()
+        )
+    }
+
+    @Test
+    fun testFilter() {
+        assertEquals(
+            listOf(2, 4),
+            Stream.of(1, 2, 3, 4).filter { x -> x % 2 == 0 }.toList()
+        )
+    }
+
+    @Test
+    fun testSample() {
+        assertEquals(
+            listOf(36, 42),
+            listOf(1, 2, 3, 4)
+                .map { it + 10 } // --> 4개 (중간 과정)
+                .filter { it % 2 == 0 } // --> 2개 (중간 과정)
+                .map { it * 3 } // --> 2개
+        )
+
+        assertEquals(
+            listOf(36, 42),
+            Stream.of(1, 2, 3, 4)
+                .map { it + 10 }
+                .filter { it % 2 == 0 }
+                .map { it * 3 } // --> 여기까지는 전부 지연. 평가를 하지 않음.
+                .toList() // --> 평가 시작.
+        )
+
+//        x -> f(g(h(x)))
+//        x -> (f.g.h)(x)
+
+        Stream.of(1, 2, 3, 4)
+            .map { it + 10 }
+            .filter { it % 2 == 0 }
+
+        Stream.cons(
+            { 1 },
+            { Stream.of(2, 3, 4) }
+        )
+            .map { it + 10 }
+            .filter { it % 2 == 0 }
+
+        Stream.cons(
+            { 1 + 10 },
+            { Stream.of(2, 3, 4).map { it + 10 } }
+        )
+            .filter { it % 2 == 0 }
+
+        Stream.of(2, 3, 4).map { it + 10 }
+            .filter { it % 2 == 0 }
+    }
+
+    @Test
+    fun testOnes() {
+        assertEquals(listOf(1, 1, 1, 1), ones().take(4).toList())
+    }
+
+    @Test
+    fun testUnfold() {
+        assertEquals(
+            listOf(0, 2, 4),
+            unfold(0) { x: Int -> Some(Pair(x * 2, x + 1)) }.take(3).toList()
         )
     }
 }
--- 20231119/kotlin/src/test/kotlin/com/example/chapter06/Chapter06Test.kt
+++ 20231119/kotlin/src/test/kotlin/com/example/chapter06/Chapter06Test.kt
@@ -1,11 +1,164 @@
 package com.example.chapter06
 
 import kotlin.test.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+
+interface RNG {
+    fun nextInt(): Pair<Int, RNG>
+}
+
+data class SimpleRNG(val seed: Long) : RNG {
+    override fun nextInt(): Pair<Int, RNG> {
+        val newSeed = (seed * 0x5DFFCE66DL + 0xBL) and 0xFFFFFFFFFFFFL
+        val nextRNG = SimpleRNG(newSeed)
+        val n = (newSeed ushr 16).toInt()
+        return Pair(n, nextRNG)
+    }
+}
+
+//typealias Rand<A> = (RNG) -> Pair<A, RNG>
+
+data class State<S, out A>(val run: (S) -> Pair<A, S>) {
+    operator fun invoke(s: S) = run(s)
+}
+
+typealias Rand<A> = State<RNG, A>
+
+fun <A> unit(a: A): Rand<A> = Rand { rng -> Pair(a, rng) }
+
+val intR: Rand<Int> = Rand { rng -> rng.nextInt() }
+
+//fun nonNegativeInt(rng: RNG): Pair<Int, RNG> {
+//    // 8 bits: -128 ~ +127
+//    // 음수면 어떻게? -> 128을 더하자. = -128을 빼자.
+//    val (x, nextRNG) = rng.nextInt()
+//    val y = if (x >= 0) x else x - Int.MIN_VALUE
+//    return Pair(y, nextRNG)
+//}
+
+val nonNegativeInt: Rand<Int> = map(intR) { x ->
+    if (x >= 0) x
+    else x - Int.MIN_VALUE
+}
+
+//fun double(rng: RNG): Pair<Double, RNG> {
+//    val (x, nextRNG) = nonNegativeInt(rng)
+//    val y = x.toDouble() / Int.MAX_VALUE
+//    return Pair(y, nextRNG)
+//}
+
+val double: Rand<Double> = map(intR) { it.toDouble() / Int.MAX_VALUE }
+
+fun ints(count: Int, rng: RNG): Pair<List<Int>, RNG> {
+    tailrec fun go(n: Int, xs: List<Int>, rng: RNG): Pair<List<Int>, RNG> {
+        if (n == 0) {
+            return Pair(xs, rng)
+        }
+        val (x, nextRNG) = rng.nextInt()
+        return go(n - 1, xs + listOf(x), nextRNG)
+    }
+
+    return go(count, listOf(), rng)
+}
+
+fun <A, B> map(s: Rand<A>, f: (A) -> B): Rand<B> = Rand { rng: RNG ->
+    val (x, nextRNG) = s(rng)
+    Pair(f(x), nextRNG)
+}
+
+val nonNegativeEven: Rand<Int> = map(nonNegativeInt) { it - it % 2 }
+
+//fun nonNegativeLessThan(n: Int): Rand<Int> = { rng ->
+//    val (x, nextRNG) = nonNegativeInt(rng)
+//    val mod = x % n
+//    if (x + (n - 1) - mod >= 0)
+//        Pair(mod, nextRNG)
+//    else
+//        nonNegativeLessThan(n)(nextRNG)
+//}
+
+fun nonNegativeLessThan(n: Int): Rand<Int> =
+    flatMap(nonNegativeInt) { x ->
+        val mod = x % n
+        if (x + (n - 1) - mod >= 0)
+            unit(mod)
+        else
+            nonNegativeLessThan(n)
+    }
+
+fun <S, A, B> flatMap(f: State<S, A>, g: (A) -> State<S, B>): State<S, B> =
+    State { s ->
+        val (x, nextInstance) = f(s)
+        g(x)(nextInstance)
+    }
 
 class Chapter06Test {
     @Test
-    fun testSimple() {
-        assertEquals(3, 1 + 2)
+    fun testSimpleRNG() {
+        val rng = SimpleRNG(1004)
+        val (x, _) = rng.nextInt()
+        val (y, _) = rng.nextInt()
+
+        assertEquals(x, y)
+
+        val (_, rng2) = rng.nextInt()
+        val (_, rng3) = rng2.nextInt()
+        val (_, _) = rng3.nextInt()
+
+        listOf(1, 2, 3)
+            .fold(Pair<List<Int>, RNG>(listOf(), rng)) { acc, _ ->
+                val (numbers, currentRNG) = acc
+                val (n, nextRNG) = currentRNG.nextInt()
+                Pair(numbers + listOf(n), nextRNG)
+            }.first
+    }
+
+    @Test
+    fun testNonNegativeInt() {
+        val rng = SimpleRNG(1004)
+
+        val (x, _) = nonNegativeInt(rng)
+
+        assertTrue(x >= 0)
+    }
+
+    @Test
+    fun testDouble() {
+        val rng = SimpleRNG(1004)
+
+        val (x, _) = double(rng)
+
+        assertTrue(x >= 0)
+        assertTrue(x <= 1)
+    }
+
+    @Test
+    fun testInts() {
+        val rng = SimpleRNG(1004)
+
+        val (xs, _) = ints(4, rng)
+
+        assertEquals(4, xs.size)
+    }
+
+    @Test
+    fun testNonNegativeEven() {
+        val rng = SimpleRNG(1004)
+
+        val (x, _) = nonNegativeEven(rng)
+
+        assertTrue(x >= 0)
+        assertTrue(x % 2 == 0)
+    }
+
+    @Test
+    fun testNonNegativeLessThan() {
+        val rng = SimpleRNG(1004)
+
+        val (x, _) = nonNegativeLessThan(10)(rng)
+
+        assertTrue(x >= 0)
+        assertTrue(x < 10)
     }
 }
