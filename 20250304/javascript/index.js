d2648680a2a956b2b92e7fc6803b1992ea4bdddd
Ashal aka JOKER <ahastudio@gmail.com>
2025-03-04 23:47:41 +0900

    createElement 추가
    DOM에서도 활용할 수 있도록 createElement 함수 추가.

--- 20250304/javascript/index.js
+++ 20250304/javascript/index.js
@@ -6,8 +6,18 @@ function generateHTML(tag, props, elements) {
   return `<${tag}${propsString}>${elements.join('')}</${tag}>`;
 }
 
+function createElement(tag, props, elements) {
+  const element = document.createElement(tag);
+  Object.assign(element, props);
+  elements.forEach((child) => {
+    element.append(child);
+  });
+  return element;
+}
+
 ['html', 'body', 'div', 'p', 'a'].forEach((tag) => {
   globalThis[tag] = (props, elements) => generateHTML(tag, props, elements);
+  // globalThis[tag] = (props, elements) => createElement(tag, props, elements);
 });
 
 console.log((
